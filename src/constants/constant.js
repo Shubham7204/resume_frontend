49e11e7d4a51395ef7c8b2d4fdc662dbf041f759
Shubham Mourya <83085067+Shubham7204@users.noreply.github.com>
2024-07-19 19:42:15 +0530

    Update constant.js

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,2 +1,2 @@
 export const BASE_URL_LOCAL = "http://localhost:3000";
-export const BASE_URL_PROD = "https://resume-backend-jfaq.onrender.com/";
+export const BASE_URL_PROD = "https://resume-backend-jfaq.onrender.com";
